fe13a4d931a61a41b2a20515c73688939daab2ab
4geru <westhouse51@gmail.com>
2022-02-11 07:20:54 +0900

    write src/pages/Top

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,47 +1,90 @@
-import React from 'react'
-import { useAuthState } from '../contexts/UserContext'
-import { SignInButton } from '../components/SignInButton'
+import React, { useEffect, useState } from 'react'
 import { SignOutButton } from '../components/SignOutButton'
+import { SignInButton } from '../components/SignInButton'
+import { SendMessagesButton } from '../components/SendMessagesButton'
 
 const Top = () => {
-  const { state } = useAuthState()
+  const [liffObject, setLiffObject] = useState<any>(null)
+  const [profileName, setProfileName] = useState<string>('')
+  const [pictureUrl, setPictureUrl] = useState<string>('')
+
+  useEffect(() => {
+    import('@line/liff').then((liff: any) => {
+      liff
+        .init({ liffId: import.meta.env.VITE_APP_LIFF_ID })
+        .then(() => {
+          setLiffObject(liff)
+          if (liff.isLoggedIn()) {
+            liff
+              .getProfile()
+              .then((profile: any) => {
+                setProfileName(profile.displayName)
+                setPictureUrl(profile.pictureUrl)
+              })
+              .catch((err: any) => {
+                console.error({ err })
+              })
+          }
+        })
+        .catch((err: any) => {
+          console.error({ err })
+        })
+    })
+  }, [])
+
+  const sendMessages = async () => {
+    await liffObject.sendMessages([
+      {
+        type: 'text',
+        text: 'Hello World',
+      },
+    ])
+  }
+  const logout = () => {
+    liffObject.logout()
+    setProfileName('')
+  }
+  const login = () => {
+    if (!liffObject.isLoggedIn()) {
+      liffObject.login({})
+    }
+  }
 
   return (
     <>
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://nekohack-inc.s3.ap-northeast-3.amazonaws.com/nekohack.svg"
-              alt="nekohack logo"
-            />
+            {liffObject?.isLoggedIn() && (
+              <img
+                className="mx-auto h-12 w-auto"
+                src={pictureUrl}
+                alt={`${profileName} logo`}
+              />
+            )}
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              {state.state === 'UNKNOWN' ? (
-                <>{'Sign in to your account'}</>
-              ) : state.state === 'SIGNED_OUT' ? (
-                <>{'Sign in to your account'}</>
+              {!liffObject?.isLoggedIn() ? (
+                <>
+                  {'Sign in to your account'}
+                  <SignInButton login={login} />
+                </>
               ) : (
-                <>{'You are signed to your account'}</>
+                <>
+                  {'You are signed to your account'}
+                  <SignOutButton logout={logout} />
+                </>
               )}
             </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              <a
-                href="https://github.com/jiyuujin/vite-react-firebase"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                please check the repository
-              </a>
-            </p>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              {liffObject?.isLoggedIn() && (
+                <SendMessagesButton sendMessages={sendMessages} />
+              )}
+            </h2>
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <div>
-              {state.state === 'UNKNOWN' ? (
-                <SignInButton />
-              ) : state.state === 'SIGNED_OUT' ? (
-                <SignInButton />
-              ) : (
-                <SignOutButton />
+              {liffObject?.isLoggedIn() && (
+                <>{`${profileName} (${liffObject?.getVersion()})`}</>
               )}
             </div>
           </form>
