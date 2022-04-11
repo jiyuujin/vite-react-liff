c6a33ff4713fcdbabfe6a8c8f29ee2586db6c484
jiyuujin <jiyuujinunite@gmail.com>
2022-04-11 12:10:19 +0900

    chore: react18, others
    - update react to 18
    - update vite to 2.9
    - update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
 
-const viteEnv = {}
-Object.keys(process.env).forEach((key) => {
-  if (key.startsWith(`VITE_`)) {
-    viteEnv[`import.meta.env.${key}`] = process.env[key]
+export default ({ mode }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
   }
-})
 
-export default defineConfig({
-  base: './',
-  root: './src',
-  build: {
-    outDir: '../out',
-    emptyOutDir: true
-  },
-  plugins: [react()],
-  define: viteEnv,
-  server: {
-    https: {
-      cert: fs.readFileSync('./certificates/localhost.pem'),
-      key: fs.readFileSync('./certificates/localhost-key.pem')
+  return defineConfig({
+    base: './',
+    root: './src',
+    build: {
+      outDir: '../out',
+      emptyOutDir: true
+    },
+    plugins: [react()],
+    server: {
+      https: {
+        cert: fs.readFileSync('./certificates/localhost.pem'),
+        key: fs.readFileSync('./certificates/localhost-key.pem')
+      }
     }
-  }
-})
+  })
+}
