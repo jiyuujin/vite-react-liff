54f56c5c990a849dcd331b073434e86c7e5be31c
jiyuujin <jiyuujinunite@gmail.com>
2022-09-18 13:06:52 +0900

    update rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     }
   },
   rules: {
-    '@typescript-eslint/no-explicit-any': 0
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-function': 0
   }
 }
